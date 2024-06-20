06b923498675cd69a38ced63cff0ac3e00ab7a5b
Aditya Eka Pranata <aepranata@gmail.com>
2024-06-21 03:30:46 +0700

    fixup! sepolicy: include flipendo/turbo_adapter policy
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -18,13 +18,13 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/afterlife/sepolicy/common/dynamic \
-    device/afterlife/sepolicy/common/system
+    device/afterlife/sepolicy/common/system \
     hardware/google/pixel-sepolicy/flipendo \
     hardware/google/pixel-sepolicy/turbo_adapter
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/afterlife/sepolicy/common/dynamic \
-    device/afterlife/sepolicy/common/vendor
+    device/afterlife/sepolicy/common/vendor \
     hardware/google/pixel-sepolicy/flipendo \
     hardware/google/pixel-sepolicy/turbo_adapter
 endif
